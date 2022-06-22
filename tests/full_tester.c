b8e3f6f632a3a77b693eeeaff395bd5aeee45eeb
Keane Moraes <keanejonathan3@gmail.com>
2022-06-22 01:01:52 -0400

    added the file write function

--- tests/full_tester.c
+++ tests/full_tester.c
@@ -38,6 +38,51 @@ int do_nothing_save(OQS_SECRET_KEY *sk) {
     return 0;
 }
 
+int sk_file_write(OQS_SECRET_KEY *sk) {
+
+    unsigned char filename[MAX_LENGTH_FILENAME] = "mine_xmss20_sha256.prv";
+
+    #ifdef CUSTOM_NAME
+    printf("\nEnter the filename that you want written to>");
+    scanf("%32s", filename);
+    strcat(filename, ".prv");
+    prepend(filename, "./keys/")
+    #endif
+
+    printf("\nWriting to file %s\n", filename);
+
+    unsigned long idx = ((unsigned long)sk->secret_key[XMSS_OID_LEN + 0] << 24) |
+                        ((unsigned long)sk->secret_key[XMSS_OID_LEN + 1] << 16) |
+                        ((unsigned long)sk->secret_key[XMSS_OID_LEN + 2] << 8) |
+                        ((unsigned long)sk->secret_key[XMSS_OID_LEN + 3]);
+
+    #ifdef DEBUGGING
+    printf("The index currently is : &%ld", idx);
+    #endif
+
+    FILE *printer = fopen(filename, "w+");
+    if (printer == NULL) {
+        perror("ERROR! There is no such file. Terminating...");
+        return -1;
+    }
+
+    // Write the entire secret key byte array to the specified file.
+    for (unsigned int i = 0; i < sk->length_secret_key; i++) {
+        
+        if (fputc(sk->secret_key[i], printer) == EOF) {
+            perror("ERROR! There is no such file. Terminating...");
+            return -1;
+        }
+
+        #ifdef DEBUGGING
+        printf("Byte being put: %02x\n", sk->secret_key[i]);
+        #endif
+    }
+    fclose(printer);
+    printf("Completed the write operation\n");
+    return 0;
+}
+
 /** =========================================================================== */
 
 
