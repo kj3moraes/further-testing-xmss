b3104252f1c9606dbd2026e5239054319cc3b983
Keane Moraes <keanejonathan3@gmail.com>
2022-06-22 18:58:18 -0400

    added the oqs hashing funcs not openssl

--- hash.c
+++ hash.c
@@ -21,36 +21,38 @@ void addr_to_bytes(unsigned char *bytes, const uint32_t addr[8])
     }
 }
 
+
 static int core_hash(const xmss_params *params,
                      unsigned char *out,
                      const unsigned char *in, unsigned long long inlen)
 {
     unsigned char tmp[64];
     if (params->n == 32 && params->func == XMSS_SHA2) {
-        SHA256(in, inlen, out);
+        OQS_SHA2_sha256(out, in, inlen);
     }
     else if(params->n < 32 && params->func == XMSS_SHA2) {
-        SHA256(in, inlen, tmp);
+        OQS_SHA2_sha256(tmp, in, inlen);
         memcpy(out, tmp, params->n);
     }
     else if (params->n <= 32 && params->func == XMSS_SHAKE) {
-        shake128(out, params->n, in, inlen);
+        OQS_SHA3_shake128(out, params->n, in, inlen);
     }
     else if (params->n == 64 && params->func == XMSS_SHA2) {
-        SHA512(in, inlen, out);
+        OQS_SHA2_sha512(out, in, inlen);
     }
     else if(params->n < 64 && params->func == XMSS_SHA2) {
-        SHA512(in, inlen, tmp);
+        OQS_SHA2_sha512(tmp, in, inlen);
         memcpy(out, tmp, params->n);
     }
     else if (params->n <= 64 && params->func == XMSS_SHAKE) {
-        shake256(out, params->n, in, inlen);
+        OQS_SHA3_shake256(out, params->n, in, inlen);
     }
     else {
         return -1;
     }
     return 0;
 }
+
 
 /*
  * Computes PRF(key, in), for a key of params->n bytes, and a 32-byte input.
